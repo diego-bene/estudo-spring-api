86d762610377f0f5bb752e7491d9558c692d57db
Diego Benedicto <diegobenedicto2014@gmail.com>
2019-08-10 14:43:26 -0300

    Primeiro Commit

---
+++ src/main/java/com/bene/estudospring/api/EstudoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bene.estudospring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringApplication.class, args);
+	}
+
+}
